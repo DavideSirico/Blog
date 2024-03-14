9d7bf28d6213fa7843e4b5e144f1567a696d9997
Andrea <104673407+andreaVMC@users.noreply.github.com>
2024-03-14 10:24:13 +0100

    Update WebSecurityConfig.java for security

--- src/main/java/blog/sirico/Blog/WebSecurityConfig.java
+++ src/main/java/blog/sirico/Blog/WebSecurityConfig.java
@@ -49,10 +49,10 @@ public class WebSecurityConfig {
 		@SuppressWarnings("deprecation")
 		UserDetails user =
 			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
+				.username("Surry")
+				.password("SurryCraft")  
 				.roles("ADMIN")
-				.build();
+				.build(); //security skill issue
 
 		return new InMemoryUserDetailsManager(user);
 	}
